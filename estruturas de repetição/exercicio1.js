0d62a3033e7bebd3cd358c5c3e66a8c700196e32
Sallenkon S. Almeida <sallenkon54@gmail.com>
2023-12-23 17:13:31 -0300

    finalizando a aula de estruturas

---
+++ estruturas de repetição/exercicio1.js
@@ -0,0 +1,10 @@
+
+let tabuada = 5;
+let conta = 0;
+
+for (let i = 1; i <= 10; i++) {
+
+    conta = tabuada * i;
+    console.log(conta);
+}
+
--- estruturas de repetição/for.js
+++ estruturas de repetição/for.js
@@ -1,3 +1,19 @@
-for(i=0; i<11; i++){
-    console.log(i);
+const notas = [];
+
+notas.push(10);
+notas.push(7);
+notas.push(5);
+notas.push(8);
+notas.push(9);
+notas.push(6);
+notas.push(4);
+
+let soma = 0;
+
+for (i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
 }
+
+const media = soma / notas.length;
+console.log(media);
